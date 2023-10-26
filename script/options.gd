10b3da65d9a3fa2f726c1d5a0494a1137e57a2aa
alfobo <132919061+alfobo@users.noreply.github.com>
2023-10-25 22:19:51 -0400

    Added two more resolution options

--- script/options.gd
+++ script/options.gd
@@ -13,18 +13,19 @@ func _on_resolution_item_selected(index):
 			resolution(1280,720)
 		2:
 			resolution(1920,1080)
+		3:
+			resolution(2560,1440)
+		4:
+			resolution(3840,2160)
 
 func _on_chroma_item_selected(index):
 	match index:
 		1:
 			emit_signal("chromaChange",null)
-			print("index1")
 		2:
 			emit_signal("chromaChange","blue")
-			print("index2")
 		3:
 			emit_signal("chromaChange","green")
-			print("index3")
 
 func _on_inputs_toggled(button_pressed):
 	emit_signal("inputData",button_pressed)
